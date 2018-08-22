880b048e467132934e590e2f262f476f31942b01
ikikika <iki.jito@gmail.com>
2018-08-22 12:48:14 +0800

    projmger create new component, link to app

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Projects from './Components/Projects';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         My App
+        <Projects />
       </div>
     );
   }
