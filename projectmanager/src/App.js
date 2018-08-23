8c55759f6a71f98702ec3cd25bfd14bc165fc234
ikikika <iki.jito@gmail.com>
2018-08-23 12:52:23 +0800

    projmger create a constructor, used to store state. state is used to store dynamic objects, usually from api or db.

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import Projects from './Components/Projects';
 
 class App extends Component {
+
+  constructor(){
+    this.state = {
+      projects: [
+        {
+          title: "Business Wwbsite",
+          category: "Web Design"
+        },
+        {
+          title: "Social App",
+          category: "Mobile Development"
+        },
+        {
+          title: "E-commerce shopping cart",
+          category: "Web Development"
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
