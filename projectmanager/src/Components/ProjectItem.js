59f90b27125de9e0bcbd98e20b874faa0d59d248
ikikika <iki.jito@gmail.com>
2018-08-24 15:10:47 +0800

    projmger access props and output

--- projectmanager/src/Components/ProjectItem.js
+++ projectmanager/src/Components/ProjectItem.js
@@ -4,7 +4,7 @@ class ProjectItem extends Component {
   render() {
     return (
       <li className="Projects">
-        
+        {this.props.project.title} - {this.props.project.category} 
       </li>
     );
   }
