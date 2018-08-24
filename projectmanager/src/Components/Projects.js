9cf3962e7f4c9858b0e7962fd663baa4fa2a3d0e
ikikika <iki.jito@gmail.com>
2018-08-24 15:08:59 +0800

    projmger assign project from props to PorjectItem component

--- projectmanager/src/Components/Projects.js
+++ projectmanager/src/Components/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
 
@@ -8,13 +9,16 @@ class Projects extends Component {
 
     if( this.props.projects ){
       projectItems = this.props.projects.map(project => {
-        console.log(project);
+        //console.log(project);
+        return (
+          <ProjectItem key={project.title} project={project}/>
+        );
       });
     }
 
     return (
       <div className="Projects">
-        My Projects
+        {projectItems}
       </div>
     );
   }
