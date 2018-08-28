401e9c1103edf0cf433bac01f2c602c316ddc9a1
ikikika <iki.jito@gmail.com>
2018-08-28 12:41:15 +0800

    import uuid and assign uuid to each item and newly added item

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import './App.css';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
@@ -15,14 +16,17 @@ class App extends Component {
   componentWillMount(){
     this.setState({projects: [
       {
+        id: uuid.v4(),
         title: "Business Wwbsite",
         category: "Web Design"
       },
       {
+        id: uuid.v4(),
         title: "Social App",
         category: "Mobile Development"
       },
       {
+        id: uuid.v4(),
         title: "E-commerce shopping cart",
         category: "Web Development"
       }
