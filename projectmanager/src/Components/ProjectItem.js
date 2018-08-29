550f1585c1270ef17374c7a8526d2187dc63c523
ikikika <iki.jito@gmail.com>
2018-08-29 16:48:50 +0800

    projmger set up delete function, bind id

--- projectmanager/src/Components/ProjectItem.js
+++ projectmanager/src/Components/ProjectItem.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class ProjectItem extends Component {
+  deleteProject(id){
+    //this.props.onDelete(id);
+    console.log(id);
+  }
+
   render() {
     return (
       <li className="Projects">
-        <strong>{this.props.project.id}</strong>: {this.props.project.category} 
+        <strong>{this.props.project.id}</strong>: {this.props.project.category}
+        <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>x</a>
       </li>
     );
   }
