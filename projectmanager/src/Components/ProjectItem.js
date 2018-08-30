b4ad4c30d76efa1b85360c922a088708ca3ca78c
ikikika <iki.jito@gmail.com>
2018-08-30 17:30:11 +0800

    projmger npm install --save 'prop-types', import and use proptypes to check if the type of the properties are correct

--- projectmanager/src/Components/ProjectItem.js
+++ projectmanager/src/Components/ProjectItem.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ProjectItem extends Component {
   deleteProject(id){
     this.props.onDelete(id);
-    // console.log(id);
   }
 
   render() {
     return (
-      <li className="Projects">
-        <strong>{this.props.project.id}</strong>: {this.props.project.category}
-        <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>x</a>
+      <li className="Project">
+        <strong>{this.props.project.title}</strong>: {this.props.project.category}
+        <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
       </li>
     );
   }
+}
+
+ProjectItem.propTypes = {
+  project: PropTypes.object,
+  onDelete: PropTypes.func
 }
 
 export default ProjectItem;
