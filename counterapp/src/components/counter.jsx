5cab64ce3d259035455a3c047b3602b42c1c62b6
ikikika <iki.jito@gmail.com>
2018-09-09 15:58:10 +0800

    count render text cynamically from a function

--- counterapp/src/components/counter.jsx
+++ counterapp/src/components/counter.jsx
@@ -9,10 +9,16 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
   }
+
+  formatCount(){
+    const {count} = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
 }
 export default Counter;
