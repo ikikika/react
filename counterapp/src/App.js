618bdaad9b9af785b53f599596d9ff01819fb623
ikikika <iki.jito@gmail.com>
2018-09-19 22:44:32 +0800

    count render app.js with navbar and counter components, encapsulate with React.Fragment

--- counterapp/src/App.js
+++ counterapp/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import NavBar from './components/navbar';
+import Counters from './components/counters';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <NavBar />
-
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
     );
   }
 }
