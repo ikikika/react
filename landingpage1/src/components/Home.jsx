2e9ed4db6cf4c4f348b7b9b1c6080d87106bc0b2
ikikika <iki.jito@gmail.com>
2018-09-29 15:06:25 +0800

    lp1 use rows and cols for home

--- landingpage1/src/components/Home.jsx
+++ landingpage1/src/components/Home.jsx
@@ -14,6 +14,28 @@ export default class Home extends Component {
                         <Button bsStyle="primary">About</Button>
                     </Link>
                 </Jumbotron>
+
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/img01.jpg" circle responsive className="profile-pic" />
+                        <h3>Ipsum</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    </Col>
+                
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/img01.jpg" circle responsive className="profile-pic" />
+                        <h3>Ipsum</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    </Col>
+
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/img01.jpg" circle responsive className="profile-pic" />
+                        <h3>Ipsum</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    </Col>
+                </Row>
+
+                
             </Grid>
         )
     }
