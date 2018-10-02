69257f15f84e4b954ac0dbe08e16176335a05e41
ikikika <iki.jito@gmail.com>
2018-10-02 10:22:49 +0800

    redux1 import redux, set up reducer, pass reducer into redux and console.log getState. console should show 'State'

--- redux1/src/index.js
+++ redux1/src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+function reducer(){
+    return 'State';
+}
+const store = createStore(reducer);
+console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
