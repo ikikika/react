7a72198c51f9673e0234dbbbaa4b09dae88e4a3c
ikikika <iki.jito@gmail.com>
2018-12-10 06:57:21 +0800

    rtx remove bundle.js, use webpack and enable hot reloading

--- react-ts/webpack.config.js
+++ react-ts/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -14,5 +15,9 @@ module.exports = {
       }
     ]
   },
-  plugins: []
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
 };
