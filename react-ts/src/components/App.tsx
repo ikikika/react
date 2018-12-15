4c66c530a66ee5d4863ac6f5ff705453d17631ba
ikikika <iki.jito@gmail.com>
2018-12-15 10:50:21 +0800

    rtx add function to form

--- react-ts/src/components/App.tsx
+++ react-ts/src/components/App.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 
 export class App extends React.Component<{}, {}> {
+  handleSubmit(e: any) {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <h1>TS List</h1>
-        <form>
+        <form onSubmit={e => this.handleSubmit(e)}>
           <input type="text" placeholder="Task Name" />
           <button type="submit">Add</button>
         </form>
