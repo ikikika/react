f9586e7d839e37ec63b503aead3e219fb843e011
ikikika <iki.jito@gmail.com>
2018-12-26 00:14:47 +0800

    rts2 boilerplate for index.tsx

---
+++ react-ts2/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react"; //need to "import *" for typescript
+import * as ReactDOM from "react-dom";
+
+const ROOT = document.querySelector(".container");
+
+ReactDOM.render(<h1>Hello</h1>, ROOT);
