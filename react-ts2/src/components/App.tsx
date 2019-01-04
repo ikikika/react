1ab23095dc3df97bbe90149f141038f201035cb4
ikikika <iki.jito@gmail.com>
2019-01-04 16:41:26 +0800

    rts2 prevent form from refreshing page after submitting

--- react-ts2/src/components/App.tsx
+++ react-ts2/src/components/App.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 
 export class App extends React.Component<{}, {}> {
   //in <>, first is props, second is state
+
+  handleSubmit(e: any) {
+    e.preventDefault();
+  }
   render() {
     return (
       <div>
         <h1>React Typescript</h1>
-        <form action="">
+        <form onSubmit={e => this.handleSubmit(e)}>
           <input type="text" placeholder="Enter task here" />
           <button type="submit">Add</button>
         </form>
