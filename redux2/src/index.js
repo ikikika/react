863472badd8af853e5d4344c2813d76b86849b19
ikikika <iki.jito@gmail.com>
2019-01-28 09:59:50 +0800

    redux2 using provider to access the store

--- redux2/src/index.js
+++ redux2/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
 function productsReducer(state = [], action) {
   return state;
 }
@@ -29,15 +30,12 @@ const store = createStore(
 );
 console.log(store.getState());
 
-const updateUserAction = {
-  type: "updateUser",
-  payload: {
-    user: "John"
-  }
-};
-store.dispatch(updateUserAction);
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
