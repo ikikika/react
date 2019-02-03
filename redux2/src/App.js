fc3b81779924b880e93794586b6c299260e0ca99
ikikika <iki.jito@gmail.com>
2019-02-03 22:13:02 +0800

    redux2 mapActionsToProps, call function to automatically dispatch actions to the store

--- redux2/src/App.js
+++ redux2/src/App.js
@@ -3,25 +3,24 @@ import logo from "./logo.svg";
 import "./App.css";
 import { connect } from "react-redux";
 
+import { updateUser } from "./actions/user-actions";
 class App extends Component {
+  //we need to access "this" in our methods, we need to bind it in the constructor
+  constructor(props) {
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+  onUpdateUser() {
+    this.props.onUpdateUser("Nami");
+  }
+
   render() {
     console.log(this.props);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div onClick={this.onUpdateUser}>update user</div>
+        {this.props.user}
       </div>
     );
   }
@@ -32,4 +31,13 @@ const mapStateToProps = state => ({
   products: state.products,
   user: state.user
 });
-export default connect(mapStateToProps)(App);
+
+//mapActionsToProps: allows us to dispatch actions easily so we don't need to dispatch actions from the components.
+// we can call functions that will automaticlaly dispatch actions to the store
+const mapActionsToProps = {
+  onUpdateUser: updateUser
+};
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(App);
