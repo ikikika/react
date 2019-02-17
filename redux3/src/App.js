d1b87ed8240a05cb5bc2fa6de740872372e27c01
ikikika <iki.jito@gmail.com>
2019-02-17 23:58:55 +0800

    redux3 import provider, wrap app, create store

--- redux3/src/App.js
+++ redux3/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 import "./App.css";
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <Postform />
         <hr />
         <Posts />
-      </div>
+      </Provider>
     );
   }
 }
