9a2dab923764e29e29b4205382b1aa872af13fb1
ikikika <iki.jito@gmail.com>
2019-03-06 15:54:31 +0800

    some home page stuff

--- landpg2/src/components/home.js
+++ landpg2/src/components/home.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
-
+import { Grid, Cell } from "react-mdl";
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="home-grid">
+          <Cell col={12}>
+            <img
+              src="https://via.placeholder.com/150"
+              alt="image"
+              className="avatar-image"
+            />
+            <hr />
+            <p>ABC | DF| GHI</p>
+          </Cell>
+        </Grid>
       </div>
     );
   }
