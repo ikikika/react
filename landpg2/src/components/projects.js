18f320d82b7bebfeeb05d5a1990c9cb5098d9827
ikikika <iki.jito@gmail.com>
2019-03-08 11:17:27 +0800

    use card to into each project

--- landpg2/src/components/projects.js
+++ landpg2/src/components/projects.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
-import { Tabs, Tab, Grid, Cell } from "react-mdl";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 class Projects extends Component {
   constructor(props) {
     super(props);
@@ -7,7 +19,28 @@ class Projects extends Component {
   }
   toggleCategories() {
     if (this.state.activeTab === 0) {
-      return <div>this is react</div>;
+      return (
+        <Card chadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background: "url(https://via.placeholder.com/150) center / cover"
+            }}
+          >
+            React Project #1
+          </CardTitle>
+          <CardText>Lorem ipsum dolor sit amer</CardText>
+          <CardActions border>
+            <Button colored>GIthub</Button>
+            <Button colored>GIthub</Button>
+            <Button colored>GIthub</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
     } else if (this.state.activeTab === 1) {
       return <div>this is angular</div>;
     } else if (this.state.activeTab === 2) {
