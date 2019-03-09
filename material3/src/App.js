ad38d0dafaf2c6ded5d55bf81527a012c2f0481d
ikikika <iki.jito@gmail.com>
2019-03-10 07:05:26 +0800

    use toolbar and typography

--- material3/src/App.js
+++ material3/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TypoGraphy from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppBar>
-          <h1>My HEader</h1>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              My Header
+            </Typography>
+          </Toolbar>
         </AppBar>
       </div>
     );
