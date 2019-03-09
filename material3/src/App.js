3cc94b710a21f93930772eecb44e130312b3da4e
ikikika <iki.jito@gmail.com>
2019-03-10 07:02:13 +0800

    add in header with default colors

--- material3/src/App.js
+++ material3/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <AppBar>
+          <h1>My HEader</h1>
+        </AppBar>
+      </div>
+    );
   }
 }
 
