6285746c1b69b875b339c943326b69a3cbef3954
ikikika <iki.jito@gmail.com>
2019-03-10 07:18:35 +0800

    create navbar and put in hime page

--- material3/src/App.js
+++ material3/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import TypoGraphy from "@material-ui/core/Typography";
 import Typography from "@material-ui/core/Typography";
+import NavBar from "./navbar";
 
 class App extends Component {
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
             <Typography variant="title" color="inherit">
               My Header
             </Typography>
+            <NavBar />
           </Toolbar>
         </AppBar>
       </div>
