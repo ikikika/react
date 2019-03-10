e670d220662a13346646a02027c5a0bbd4aa91bd
ikikika <iki.jito@gmail.com>
2019-03-10 14:19:09 +0800

    set up app container component (class) and state

--- todo1/src/App.js
+++ todo1/src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [{ id: 1, content: "do this" }, { id: 2, content: "do that" }]
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
