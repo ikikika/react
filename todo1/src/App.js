b0c09211d7ba76f3066720914fe7ac2d5975efd4
ikikika <iki.jito@gmail.com>
2019-03-12 15:25:23 +0800

    define deleteTodo function in container component as state is there, then pass function to ui as prop

--- todo1/src/App.js
+++ todo1/src/App.js
@@ -5,11 +5,17 @@ class App extends Component {
     todos: [{ id: 1, content: "do this" }, { id: 2, content: "do that" }]
   };
 
+  // this function has to be in container component because the state is here
+  // define the function and pass to ui component as prop
+  deleteTodo = id => {
+    console.log(id);
+  };
+
   render() {
     return (
       <div className="container">
         <h1 className="center blue-text">List</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
