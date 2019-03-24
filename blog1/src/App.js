6a681335c71513744aff84ffeab3edaaf4deacca
ikikika <iki.jito@gmail.com>
2019-03-24 12:52:39 +0800

    set up basic pages and navbar

--- blog1/src/App.js
+++ blog1/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>app</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ blog1/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper red arken-3">
+      <div className="container">
+        <a className="brand-logo" href="/">
+          Blog
+        </a>
+        <ul className="right">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
