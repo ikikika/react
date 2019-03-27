babf2c45ac7002b092d2822daa63930c381c656a
ikikika <iki.jito@gmail.com>
2019-03-27 13:02:22 +0800

    set up router and simple redirect to pages

--- blog1/src/App.js
+++ blog1/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    // surround entire application code with browser router, then we cna add routes inside app
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </BrowserRouter>
   );
 }
 
