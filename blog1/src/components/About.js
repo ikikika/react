07af483a340df8c148d1ea6deca4fdb4bd0a23a4
ikikika <iki.jito@gmail.com>
2019-03-29 09:41:38 +0800

    higher order component

--- blog1/src/components/About.js
+++ blog1/src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Rainbow from "../hoc/Rainbow";
 
 const About = () => {
   return (
@@ -19,4 +20,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Rainbow(About);
