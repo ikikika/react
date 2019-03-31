91328eb18dc659031bdb6cf33d660306433aa3e6
ikikika <iki.jito@gmail.com>
2019-03-31 20:35:17 +0800

    output posts

--- blog1/src/components/Home.js
+++ blog1/src/components/Home.js
@@ -2,28 +2,36 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Home extends Component {
+  state = {
+    posts: []
+  };
   // use lifecycle hooks componentDidMount
   // will be loaded before the DOM is rendered
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(result => {
-      console.log(result);
+      this.setState({ posts: result.data.slice(1, 10) });
     });
   }
   render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card=content">
+              <div className="card-title">{post.title}</div>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No Posts yet</div>
+    );
     return (
       <div className="container">
         <h4 className="center">Home</h4>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        {postList}
       </div>
     );
   }
