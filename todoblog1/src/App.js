d76846a7e17ac647f10b8c4b8fdfb61e15157d80
ikikika <iki.jito@gmail.com>
2019-04-06 06:43:01 +0800

    create navbar

--- todoblog1/src/App.js
+++ todoblog1/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="container">
-      <h1>App</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
