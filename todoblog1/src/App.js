d8f2f65ffc51eb8c23aa37a42cec39fd0c8f1387
ikikika <iki.jito@gmail.com>
2019-04-09 04:20:01 +0800

    load post on individual page

--- todoblog1/src/App.js
+++ todoblog1/src/App.js
@@ -4,16 +4,20 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Navbar from "./components/Navbar";
 import Todo from "./components/todo/index";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Post from "./components/pages/Post";
 
 function App() {
   return (
     <div>
       <Router>
         <Navbar />
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/todo" component={Todo} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/todo" component={Todo} />
+          <Route path="/:id" component={Post} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ todoblog1/src/components/pages/Post.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Post extends Component {
+  state = {
+    post: null
+  };
+  componentDidMount() {
+    axios
+      .get(
+        "https://jsonplaceholder.typicode.com/posts/" +
+          this.props.match.params.id
+      )
+      .then(result => {
+        // console.log(result.data);
+        this.setState({ post: result.data });
+      });
+  }
+  render() {
+    const post = this.state.post ? (
+      <React.Fragment>
+        <h1>{this.state.post.title}</h1>
+        <p>{this.state.post.body}</p>
+      </React.Fragment>
+    ) : (
+      <p>loading</p>
+    );
+    console.log(this.state.post);
+    return <div className="container">{post}</div>;
+  }
+}
+
+export default Post;
