5ef6e13de51e9d885d2d8ac94d8039a7791eb93c
ikikika <iki.jito@gmail.com>
2019-04-11 19:41:15 +0800

    add new todo

--- todoblog1/src/components/todo/index.js
+++ todoblog1/src/components/todo/index.js
@@ -5,13 +5,25 @@ class Todo extends Component {
     todos: [
       { id: 1, content: "asdas sdfsfs" },
       { id: 2, content: "fsd7839hdiu sdfsfs" }
-    ]
+    ],
+    newTodo: ""
   };
   deleteTodo = id => {
     const todos = this.state.todos.filter(todo => {
       return todo.id !== id;
     });
     this.setState({ todos });
+  };
+  handleChange = e => {
+    this.setState({ ...this.state, newTodo: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const todos = [
+      ...this.state.todos,
+      { id: Date.now(), content: this.state.newTodo }
+    ];
+    this.setState({ todos, newTodo: "" });
   };
   render() {
     const todoList = this.state.todos.length ? (
@@ -46,6 +58,25 @@ class Todo extends Component {
           </thead>
           <tbody>{todoList}</tbody>
         </table>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="input-field col s12 m10">
+              <input
+                id="todoitem"
+                type="text"
+                className="validate"
+                value={this.state.newTodo}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="todoitem">Todo Item</label>
+            </div>
+            <div className="input-field col s12 m2">
+              <button className="waves-effect waves-light btn" type="submit">
+                <i className="material-icons left">add_box</i>
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
