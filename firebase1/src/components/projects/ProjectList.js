6afbce4e92a537eef1502fff9637fbb50a0eebb6
ikikika <iki.jito@gmail.com>
2019-04-25 10:42:40 +0800

    project list component

---
+++ firebase1/src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content gret-text text-darken">
+          <span className="card-title">Project Title</span>
+          <p>POsted by: NNNN</p>
+          <p className="grey-text">fdsfsd fsdfdsfds </p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content gret-text text-darken">
+          <span className="card-title">Project Title</span>
+          <p>POsted by: NNNN</p>
+          <p className="grey-text">fdsfsd fsdfdsfds </p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content gret-text text-darken">
+          <span className="card-title">Project Title</span>
+          <p>POsted by: NNNN</p>
+          <p className="grey-text">fdsfsd fsdfdsfds </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
