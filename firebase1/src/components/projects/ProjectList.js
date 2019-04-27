2783428b6c981da946043046cb78015c0b0654bd
ikikika <iki.jito@gmail.com>
2019-04-27 11:15:14 +0800

    project sumamry component

--- firebase1/src/components/projects/ProjectList.js
+++ firebase1/src/components/projects/ProjectList.js
@@ -1,31 +1,10 @@
 import React from "react";
+import ProjectSumamry from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content gret-text text-darken">
-          <span className="card-title">Project Title</span>
-          <p>POsted by: NNNN</p>
-          <p className="grey-text">fdsfsd fsdfdsfds </p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content gret-text text-darken">
-          <span className="card-title">Project Title</span>
-          <p>POsted by: NNNN</p>
-          <p className="grey-text">fdsfsd fsdfdsfds </p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content gret-text text-darken">
-          <span className="card-title">Project Title</span>
-          <p>POsted by: NNNN</p>
-          <p className="grey-text">fdsfsd fsdfdsfds </p>
-        </div>
-      </div>
+      <ProjectSumamry />
     </div>
   );
 };
