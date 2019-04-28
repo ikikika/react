a6e573dc237f2da4edf8a4ee6bc545164e625ab8
ikikika <iki.jito@gmail.com>
2019-04-28 11:27:11 +0800

    project detail component

---
+++ firebase1/src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>lorem idfsd fdfdsgdsg dfgdf </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by: safdsa</div>
+          <div>2nd sept</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
