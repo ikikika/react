aff76816d9a76d5ac10d48e61e2bde666795cf95
ikikika <iki.jito@gmail.com>
2019-05-03 11:55:57 +0800

    sign up component

---
+++ firebase1/src/components/auth/SignUp.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  state = {
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: ""
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col s12 m4 offset-m4">
+            <form className="white" onSubmit={this.handleSubmit}>
+              <h5 className="grey-text text-darken-3 center">Sign Up</h5>
+              <div className="input-field">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" onChange={this.handleChange} />
+              </div>
+              <div className="input-field">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="input-field">
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" id="lastName" onChange={this.handleChange} />
+              </div>
+              <div className="input-field">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="input-field center">
+                <button className="btn pink lighten-1 z-depth-0">Log In</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
