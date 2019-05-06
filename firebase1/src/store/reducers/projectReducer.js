c0c1e324ccf55b6e5d73b8b1850ba97aaae2ce86
ikikika <iki.jito@gmail.com>
2019-05-07 02:18:07 +0800

    access store from component using mapStateToProps

--- firebase1/src/store/reducers/projectReducer.js
+++ firebase1/src/store/reducers/projectReducer.js
@@ -1,4 +1,18 @@
-const initState = {};
+const initState = {
+  projects: [
+    {
+      id: 1,
+      title: "abcde",
+      content: "lkdshfjk sdfhsoifdh fasjfd sndf iusndfiundsf dfuin"
+    },
+    { id: 2, title: "12345", content: "l123 123 213 2432 54 543 54 423 4 2" },
+    {
+      id: 3,
+      title: "abc123",
+      content: "adfwed23 d3 3f f34f w 3 f4 f43 f43 f wf 4 "
+    }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
