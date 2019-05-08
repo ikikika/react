2b18db24364ac8894aae573515a1d2cea30805bc
ikikika <iki.jito@gmail.com>
2019-05-08 14:27:49 +0800

    output project summary from props

--- firebase1/src/components/projects/ProjectSummary.js
+++ firebase1/src/components/projects/ProjectSummary.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ projectFromStore }) => {
+  const project = projectFromStore;
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content gret-text text-darken">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>POsted by: NNNN</p>
-        <p className="grey-text">fdsfsd fsdfdsfds </p>
+        <p className="grey-text">{project.content}</p>
       </div>
     </div>
   );
