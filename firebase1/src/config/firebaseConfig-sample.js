2d6cbdb26e1e4f674378b3e2fa3a4a768dae1fb8
ikikika <iki.jito@gmail.com>
2019-05-14 19:41:36 +0800

    remove firestore settings

--- firebase1/src/config/firebaseConfig-sample.js
+++ firebase1/src/config/firebaseConfig-sample.js
@@ -15,6 +15,4 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-firebase.firestore().settings({ timestampsInSnapshots: true });
-
 export default firebase;
