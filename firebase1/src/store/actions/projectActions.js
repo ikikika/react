3cc02c002d51a0f1a2fa90f6b8390c7a51bc069c
ikikika <iki.jito@gmail.com>
2019-05-14 19:33:46 +0800

    add project, save to firebase

--- firebase1/src/store/actions/projectActions.js
+++ firebase1/src/store/actions/projectActions.js
@@ -1,7 +1,23 @@
+import { getFirestore } from "redux-firestore";
+
 export const createProject = project => {
   return (dispatch, getState) => {
     // make async call to db
-
-    dispatch({ type: "CREATE_PROJECT", project });
+    const firestore = getFirestore();
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "afn",
+        authorLastName: "aln",
+        authorId: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_PROJECT_ERR", err });
+      });
   };
 };
