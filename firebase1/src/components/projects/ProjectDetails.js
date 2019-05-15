66630abf8fd877408100ca942ccb0858a08e1a1d
ikikika <iki.jito@gmail.com>
2019-05-15 18:57:11 +0800

    get project details in props

--- firebase1/src/components/projects/ProjectDetails.js
+++ firebase1/src/components/projects/ProjectDetails.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const ProjectDetails = props => {
   const id = props.match.params.id;
+  console.log(props);
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title - {id}</span>
+          <span className="card-title">Project Title - {id} </span>
           <p>lorem idfsd fdfdsgdsg dfgdf </p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
@@ -18,4 +22,16 @@ const ProjectDetails = props => {
   );
 };
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null;
+  return {
+    project: project
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
+)(ProjectDetails);
