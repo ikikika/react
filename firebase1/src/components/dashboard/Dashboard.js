c7c9ba7f9be3c3fd440f6673c2cb1207a3c1a12d
ikikika <iki.jito@gmail.com>
2019-05-15 15:09:45 +0800

    output data from db to ui

--- firebase1/src/components/dashboard/Dashboard.js
+++ firebase1/src/components/dashboard/Dashboard.js
@@ -24,9 +24,9 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => {
-  console.log(state);
+  // console.log(state);
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   };
 };
 export default compose(
