db47d7b8b73ab55703514afa19d85b17366b4455
ikikika <iki.jito@gmail.com>
2019-05-16 22:13:49 +0800

    create user, insert uid in user collections, complete sign up function

--- firebase1/src/components/auth/SignUp.js
+++ firebase1/src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { signUp } from "../../store/actions/authActions";
 
 class SignUp extends Component {
   state = {
@@ -16,10 +17,11 @@ class SignUp extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    // console.log(this.state);
+    this.props.signUp(this.state);
   };
   render() {
-    const { auth } = this.props;
+    const { auth, authError } = this.props;
     if (auth.uid) return <Redirect to="/" />;
 
     return (
@@ -57,6 +59,9 @@ class SignUp extends Component {
                   Sign Up
                 </button>
               </div>
+              <div className="red-text center">
+                {authError ? <p>{authError}</p> : null}
+              </div>
             </form>
           </div>
         </div>
@@ -67,8 +72,18 @@ class SignUp extends Component {
 
 const mapStateToProps = state => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   };
 };
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = dispatch => {
+  return {
+    signUp: newUser => dispatch(signUp(newUser))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignUp);
