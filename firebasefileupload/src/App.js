cdda43e1b7e661263252e1436e689471b52dda36
ikikika <iki.jito@gmail.com>
2019-05-22 02:51:27 +0800

    upload file to firebase

--- firebasefileupload/src/App.js
+++ firebasefileupload/src/App.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 import firebase from "firebase";
 import FileUploader from "react-firebase-file-uploader";
 import config from "./firebaseconfig";
 
-firebase.initializeApp();
+firebase.initializeApp(config);
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  state = {
+    image: "",
+    imageURL: "",
+    progress: 0
+  };
+  handleUploadStart = () => {
+    this.setState({
+      progress: 0
+    });
+  };
+  handleUploadSuccess = filename => {
+    this.setState({
+      image: filename,
+      progress: 100
+    });
+    firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ imageURL: url }));
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <FileUploader
+          accept="image/*"
+          name="image"
+          storageRef={firebase.storage().ref("images")}
+          onUploadStart={this.handleUploadStart}
+          onUploadSuccess={this.handleUploadSuccess}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
