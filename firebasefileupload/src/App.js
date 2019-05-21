3eaa20efb411d68aa38bc30fb4caa6c5dae4185e
ikikika <iki.jito@gmail.com>
2019-05-22 02:37:22 +0800

    create and set up firebase

---
+++ firebasefileupload/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
+import config from "./firebaseconfig";
+
+firebase.initializeApp();
+
+function App() {
+  return <div className="App" />;
+}
+
+export default App;
