aabfd3958a0efaf25b73e75f63ccd7b25936c054
ikikika <iki.jito@gmail.com>
2019-05-25 01:26:27 +0800

    doesnt work

--- firebaseauth/src/App.js
+++ firebaseauth/src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import firebaseInit from "./firebaseConfig";
+import Home from "./Home";
+import Login from "./Login";
 
-function App() {
-  return <div className="App">test</div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    firebaseInit.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        // localStorage.setItem("user", user.uid);
+      } else {
+        this.setState({ user: null });
+        // localStorage.removeItem("user");
+      }
+    });
+  }
+
+  render() {
+    // console.log(this.state);
+    return <div className="App">{this.state.user ? <Home /> : <Login />}</div>;
+  }
 }
 
 export default App;
