4a6233a50bb05abf9c3fbe15a312df355be52baf
ikikika <iki.jito@gmail.com>
2019-09-28 09:54:30 +0800

    new type and action

--- redux5/src/actions/employeeActions.js
+++ redux5/src/actions/employeeActions.js
@@ -1,4 +1,4 @@
-import { FETCH_EMPLOYEES } from "./types";
+import { FETCH_EMPLOYEES, NEW_EMPLOYEE } from "./types";
 
 export const fetchEmployees = () => {
   return async dispatch => {
@@ -6,6 +6,25 @@ export const fetchEmployees = () => {
       .then(res => res.json())
       .then(employees =>
         dispatch({ type: FETCH_EMPLOYEES, payload: employees })
+      );
+  };
+};
+
+export const createEmployees = data => {
+  return async dispatch => {
+    fetch("http://dummy.restapiexample.com/api/v1/create", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(employee =>
+        dispatch({
+          type: NEW_EMPLOYEE,
+          payload: employee
+        })
       );
   };
 };
