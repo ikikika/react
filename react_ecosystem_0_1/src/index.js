1755de9aa7b9c99357745599f81666be915cffc4
ikikika <iki.jito@gmail.com>
2020-06-05 22:31:22 +0800

    setting up redux

--- react_ecosystem_0_1/src/index.js
+++ react_ecosystem_0_1/src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from "react-dom";
 
 import App from "./App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { configureStore } from "./store";
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
