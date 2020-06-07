2eb3186c5884df20d865ff67016a7e54679bdb02
ikikika <iki.jito@gmail.com>
2020-06-07 23:51:58 +0800

    refactor code to accept change in state items using selectors

--- react_ecosystem_0_1/src/store.js
+++ react_ecosystem_0_1/src/store.js
@@ -5,9 +5,9 @@ import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import { todos, isLoading } from "./todos/reducers";
+import { todos } from "./todos/reducers";
 
-const reducers = { todos, isLoading };
+const reducers = { todos };
 
 const persistConfig = {
   key: "root",
--- react_ecosystem_0_1/src/todos/selectors.js
+++ react_ecosystem_0_1/src/todos/selectors.js
@@ -1,3 +1,3 @@
-export const getTodos = (state) => state.todos;
+export const getTodos = (state) => state.todos.data;
 
-export const getTodosLoading = (state) => state.isLoading;
+export const getTodosLoading = (state) => state.todos.isLoading;
