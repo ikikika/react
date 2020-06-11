3e8f30c223900efffa7566e00cd6e49f45fa1b00
ikikika <iki.jito@gmail.com>
2020-06-11 13:18:33 +0800

    custom hooks for forms and error handling functions

---
+++ redux_auth/src/helpers/displayError.js
@@ -0,0 +1,31 @@
+export const errorMessage = (dataObject) => {
+  let str = "";
+  for (const objects in dataObject) {
+    if (typeof dataObject[objects][0] == "string") {
+      str +=
+        "<div class='alert alert-danger add-padding'>" +
+        dataObject[objects] +
+        "</div>";
+    } else {
+      str +=
+        "<div class='alert alert-danger add-padding'>" +
+        (dataObject[objects] + "</div>");
+    }
+  }
+};
+
+export const displayErrorMEssages = (errorResponse, displayError) => {
+  if (typeof errorResponse == "string") {
+    displayError.innerHTML =
+      "<div class='alert alert-danger add-padding'>" + errorResponse + "</div>";
+  } else if (typeof errorResponse == "undefined") {
+    displayError.innerHTML =
+      "<div class='alert alert-danger add-padding'>action failed error</div>";
+  } else {
+    const lengthErrorObject = Object.keys(errorResponse).length;
+    if (lengthErrorObject > 0) {
+      const err = errorMessage(errorResponse);
+      displayError.innerHTML = err;
+    }
+  }
+};
