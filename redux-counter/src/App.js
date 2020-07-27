b2780c795d861c2658732a04f2133518f5092593
ikikika <iki.jito@gmail.com>
2020-07-27 23:49:56 +0800

    dispatch action

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -6,7 +6,12 @@ function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
-  return <div>react</div>;
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+    </div>
+  );
 }
 
 export default App;
