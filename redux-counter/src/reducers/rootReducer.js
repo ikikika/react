79fa7b1b9b174cb15b363de2ff2f044b00cd53b2
ikikika <iki.jito@gmail.com>
2020-07-27 23:42:13 +0800

    basic redux set up

---
+++ redux-counter/src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+const initialState = {};
+
+function rootReducer(state = initialState, action) {
+  // this function takes 2 arguments, state and action
+  return state;
+}
+
+export default rootReducer;
