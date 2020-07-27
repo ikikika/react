b0e8a627a3e69dd8e8aeed68d6bb059b6c51a7e3
ikikika <iki.jito@gmail.com>
2020-07-27 23:45:03 +0800

    define structure of state, useSelector and useDispatch

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return <div>react</div>;
 }
 
--- redux-counter/src/reducers/rootReducer.js
+++ redux-counter/src/reducers/rootReducer.js
@@ -1,4 +1,6 @@
-const initialState = {};
+const initialState = {
+  counter: 0,
+};
 
 function rootReducer(state = initialState, action) {
   // this function takes 2 arguments, state and action
