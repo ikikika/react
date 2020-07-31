29606eb39cfa78224afbc71786973bf9b18900bb
ikikika <iki.jito@gmail.com>
2020-07-31 14:20:55 +0800

    clear to try again

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,17 +1,7 @@
 import React from "react";
 
-import { useSelector, useDispatch } from "react-redux";
-
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
-
-  return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
-    </div>
-  );
+  return <div>app</div>;
 }
 
 export default App;
--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./reducers/rootReducer";
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
