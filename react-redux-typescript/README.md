98c1088baafd4848ca2847b1fe149310ef360ab8
ikikika <iki.jito@gmail.com>
2020-08-13 23:42:34 +0800

    inital template set up

---
+++ react-redux-typescript/README.md
@@ -0,0 +1,3 @@
+1. npx create-react-app --template typescript <name>
+2. npm i redux react-redux
+3. npm i @types/react-redux
---
+++ react-redux-typescript/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>react redux typescript</div>;
+}
+
+export default App;
