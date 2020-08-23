d2368650ebd9f4de0a00539982f8ce2c46657440
ikikika <iki.jito@gmail.com>
2020-08-24 03:12:42 +0800

    initial server set up

---
+++ auth1/server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+// App set up
+
+// Server set up
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+
+console.log("Server listening on " + port);
