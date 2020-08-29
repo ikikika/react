ef97b07e708ef4dc42d8f72b9e22c696b7a075e2
ikikika <iki.jito@gmail.com>
2020-08-29 23:31:29 +0800

    conenct to db

--- auth1/server/index.js
+++ auth1/server/index.js
@@ -6,6 +6,20 @@ const morgan = require("morgan");
 const app = express();
 const router = require("./router");
 
+const mongoose = require("mongoose");
+
+// DB setup
+mongoose.connect("mongodb://localhost:auth/auth", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", function () {
+  console.log("connected to db");
+});
+
 // App set up
 // // logging framework
 app.use(morgan("combined"));
