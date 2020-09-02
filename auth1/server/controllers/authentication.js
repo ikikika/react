76e2cc3cfc379be13396ea77165c186d15fd2331
ikikika <iki.jito@gmail.com>
2020-09-02 23:15:07 +0800

    create jwt token

--- auth1/server/controllers/authentication.js
+++ auth1/server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require("jwt-simple");
 const User = require("../models/user");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function (req, res, next) {
   //   res.send({ success: true });
@@ -36,6 +43,6 @@ exports.signup = function (req, res, next) {
     });
 
     // respond to request indicating user was created
-    res.json({ success: true });
+    res.json({ token: tokenForUser(user) });
   });
 };
