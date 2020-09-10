9d6803f3bec14fd2d7d57f8eb20b5e4937f09872
ikikika <iki.jito@gmail.com>
2020-09-10 23:25:01 +0800

    set up local strategy

--- auth1/server/services/passport.js
+++ auth1/server/services/passport.js
@@ -3,6 +3,29 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+// create local strategy
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function (
+  email,
+  password,
+  done
+) {
+  // Verify email and passsword, call done with user
+  // if it is the correct email and password
+  // otherwise call done with false
+  User.findOne({ email: email }, function (err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+
+    // compare password entered with password in db
+  });
+});
 
 // Set up options for JWT strategy
 const jwtOptions = {
