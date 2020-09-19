1fcd06d16407d5abe8fcae9316503731069f900f
ikikika <iki.jito@gmail.com>
2020-09-19 15:01:00 +0800

    set up reduc

--- auth1/client/src/index.js
+++ auth1/client/src/index.js
@@ -1,19 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/Auth/Signup";
 import * as serviceWorker from "./serviceWorker";
+import reducers from "./reducers";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App>
-        <Route path="/" exact component={Welcome} />
-        <Route path="/signup" exact component={Signup} />
-      </App>
-    </BrowserRouter>
+    <Provider store={createStore(reducers, {})}>
+      <BrowserRouter>
+        <App>
+          <Route path="/" exact component={Welcome} />
+          <Route path="/signup" exact component={Signup} />
+        </App>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
