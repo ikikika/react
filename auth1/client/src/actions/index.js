a59606b82191d54241c5f0bd22e9ce28bde29d18
ikikika <iki.jito@gmail.com>
2020-10-03 00:40:55 +0800

    dispatch sign up action

--- auth1/client/src/actions/index.js
+++ auth1/client/src/actions/index.js
@@ -2,7 +2,12 @@ import { AUTH_USER } from "./types";
 import axios from "axios";
 
 export const signup = ({ email, password }) => {
-  return (dispatch) => {
-    axios.post("http://localhost:3090/signup", { email, password });
+  return async (dispatch) => {
+    const response = await axios.post("http://localhost:3090/signup", {
+      email,
+      password,
+    });
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
   };
 };
