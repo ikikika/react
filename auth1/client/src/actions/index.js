2213bb343079c58e18bf6f21bf9f6b58dd249b15
ikikika <iki.jito@gmail.com>
2020-10-05 03:47:58 +0800

    redirect on sign up

--- auth1/client/src/actions/index.js
+++ auth1/client/src/actions/index.js
@@ -1,7 +1,7 @@
 import { AUTH_USER, AUTH_ERROR } from "./types";
 import axios from "axios";
 
-export const signup = ({ email, password }) => {
+export const signup = ({ email, password }, callback) => {
   return async (dispatch) => {
     try {
       const response = await axios.post("http://localhost:3090/signup", {
@@ -10,6 +10,7 @@ export const signup = ({ email, password }) => {
       });
 
       dispatch({ type: AUTH_USER, payload: response.data.token });
+      callback();
     } catch (e) {
       dispatch({ type: AUTH_ERROR, payload: "Email in use" });
     }
